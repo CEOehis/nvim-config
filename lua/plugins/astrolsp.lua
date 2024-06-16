a81f2f9f96b4a45c0c3aed89b8b08ae6658cc070
Celestine Ekoh-Ordan <25967737+CEOehis@users.noreply.github.com>
2024-06-16 17:30:21 +0200

    migrate to v4

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -86,6 +84,15 @@ return {
           function() vim.lsp.buf.declaration() end,
           desc = "Declaration of current symbol",
           cond = "textDocument/declaration",
+        },
+        -- Override `gd` with similar functionality as astroNvim v3 (lsp.lua)
+        gd = {
+          function() require("telescope.builtin").lsp_definitions() end,
+          desc = "Go to definition",
+          cond = function(client)
+            return client.supports_method "textDocument/definition"
+              and require("astrocore").is_available "telescope.nvim"
+          end,
         },
         ["<Leader>uY"] = {
           function() require("astrolsp.toggles").buffer_semantic_tokens() end,
