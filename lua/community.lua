df191d7eac938a285a71e73cf3b932e85bbb0b31
Celestine Ekoh-Ordan <25967737+CEOehis@users.noreply.github.com>
2024-06-22 10:23:37 +0200

    add community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -5,20 +5,28 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.color.twilight-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.completion.codeium-nvim" },
   -- { import = "astrocommunity.completion.codeium-vim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.lsp.actions-preview-nvim" },
   { import = "astrocommunity.media.codesnap-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.ruby" },
   { import = "astrocommunity.pack.prisma" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.startup.mini-starter" },
+  { import = "astrocommunity.terminal-integration.flatten-nvim" },
   -- import/override with your plugins folder
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -7,13 +7,13 @@ return {
   -- == Examples of Adding Plugins ==
 
   { "wakatime/vim-wakatime", lazy = false },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-    ft = { "markdown" },
-  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   build = "cd app && yarn install",
+  --   init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+  --   ft = { "markdown" },
+  -- },
 
   -- == Examples of Overriding Plugins ==
 
